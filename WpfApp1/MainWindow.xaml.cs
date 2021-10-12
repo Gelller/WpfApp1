ec7203b7730747356052404db938efd7d9dc0e3e
Gelller <geller.ggs@gmail.com>
2021-10-12 10:34:12 +0300

    lesson-1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -15,21 +16,104 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 
+
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+
+
+        public List<MyList> someList = new List<MyList>();
+        public int interval;
         public MainWindow()
         {
             InitializeComponent();
 
 
+            for (int i = 0; i < 50; i++)
+            {
+
+                Thread function = new Thread(() => Fibonacci(i));
+                function.Start();
+              //  function.Interrupt();
+                function.Join();
+
+            }
+
+            Thread f = new Thread(() => ListAdd("qwe",1));
+            Thread q = new Thread(ListDelete);
+            f.Start();
+            f.Join();
+            q.Start();
+
         }
 
-  
+
+
+
+        private void Slider_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            interval= (Convert.ToInt32(Slider.Value));
+        }
+
+
+        public void ListAdd(string str, int n)
+        {
+            MyList NewItem = new MyList();
+
+            NewItem.someInt = n;
+            NewItem.someString = str;
+
+            someList.Add(NewItem);
+            someList.Add(NewItem);
+            someList.Add(NewItem);
+
+        }
+
+        public void ListDelete()
+        {
+            someList.RemoveAt(0);             
+        }
+
+
+
+
+        public void Fibonacci(int n)
+        {
+            try
+            {
+               // Thread.Sleep(Timeout.Infinite);
+                Application.Current.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
+                {
+               
+                        int a = 0;
+                    int b = 1;
+                    int tmp;
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        tmp = a;
+                        a = b;
+                        b += tmp;
+                    }
+                        
+                        Thread.Sleep(interval);
+                        TextBox.Text = a.ToString();
+                   
+                }));
+            }
+
+            catch (ThreadInterruptedException e)
+            {
+                        Trace.WriteLine("Thread cannot go to sleep - " +
+                            "interrupted by main thread.");
+            }
+            
+        }   
     }
 
 }
